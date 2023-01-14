8799c951f74a489df0a742c84e87bd5a8e1d0dce
omar saadeh <omarsaadeh11@gmail.com>
2023-01-14 11:12:06 -0800

    Dealing with node js File System

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-const fs = require("fs");
-const content = fs.readFileSync("./text.txt");
-console.log(content);
-console.log(content.toString("utf-8"));
+const fs = require("fs/promises");
+
+(async () => {
+  const createFile = async (path) => {
+    existingFileHandle = await fs.open(path, "r");
+    // fs.writeFile("newfile.txt", "");
+  };
+
+  //commands
+  const CREATE_FILE = "create a file";
+
+  const commandFileHandler = await fs.open("./command.txt", "r");
+
+  commandFileHandler.on("change", async () => {
+    const size = (await commandFileHandler.stat()).size;
+    const buff = Buffer.alloc(size);
+    const offset = 0;
+    const length = buff.byteLength;
+    const position = 0;
+    await commandFileHandler.read(buff, offset, length, position);
+    const command = buff.toString("utf-8");
+    // create a file:
+    // create a file <path>
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE + 1);
+      createFile(filePath);
+    }
+  });
+
+  // watcher...
+  const watcher = fs.watch("./command.txt");
+  for await (const event of watcher) {
+    if (event.eventType === "change") {
+      commandFileHandler.emit("change");
+    }
+  }
+})();
